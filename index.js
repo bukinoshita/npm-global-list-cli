f60f43bf8293ebd118e99b1792695042f5fd5961
Bu Kinoshita <bukinoshita@gmail.com>
2017-09-06 21:27:02 -0400

    ngl

---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+'use strict'
+
+const meow = require('meow')
+const updateNotifier = require('update-notifier')
+const npmGlobalList = require('npm-global-list')
+const shoutSuccess = require('shout-success')
+
+const cli = meow(
+  `
+  Usage:
+    $ ngl
+    $ ngl -l=100
+
+  Options:
+    -l, --limit                 Limit
+    -h, --help                  Show help options
+    -v, --version               Show version
+`,
+  {
+    alias: {
+      l: 'limit',
+      h: 'help',
+      v: 'version'
+    }
+  }
+)
+
+updateNotifier({ pkg: cli.pkg }).notify()
+
+npmGlobalList().then(pkgs => {
+  shoutSuccess(`You have ${pkgs.length} packages installed globally.\n`)
+  const l = cli.flags.limit || 11
+  const limit = l > pkgs.length ? pkgs.length : l
+
+  for (let index = 0; index <= limit; index++) {
+    if (index === limit) {
+      return false
+    }
+
+    console.log(`⇢ ${pkgs[index]}`)
+  }
+})
---
+++ readme.md
@@ -0,0 +1,36 @@
+# npm-global-list-cli [![Build Status](https://travis-ci.org/bukinoshita/npm-global-list-cli.svg?branch=master)](https://travis-ci.org/bukinoshita/npm-global-list-clit)
+
+> Get all npm packages installed globally
+
+
+## Install
+
+```bash
+$ npm install -g npm-global-list-cli
+```
+
+
+## Usage
+
+```bash
+$ ngl --help
+
+  Usage:
+    $ ngl
+    $ ngl -l=100
+
+  Options:
+    -l, --limit                 Limit
+    -h, --help                  Show help options
+    -v, --version               Show version
+```
+
+
+## Related
+
+- [npm-global-list](https://github.com/bukinoshita/npm-global-list) — API for this module
+
+
+## License
+
+MIT © [Bu Kinoshita](https://bukinoshita.io)
